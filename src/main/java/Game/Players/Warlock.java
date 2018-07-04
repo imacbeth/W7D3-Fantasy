c2dd99deb494b68d537a2eb6393df2939eb5d2aa
James Henderson <hendersonjamesalexander@gmail.com>
2018-07-04 13:50:53 +0100

    all players created, tests all passing

--- src/main/java/Game/Players/Warlock.java
+++ src/main/java/Game/Players/Warlock.java
@@ -1,4 +1,19 @@
 package Game.Players;
 
-public class Warlock {
+import Game.Abilities.Spell;
+import Game.Behaviours.IPlayer;
+import Game.Behaviours.ITreasure;
+
+import java.util.ArrayList;
+
+public class Warlock extends SpellCaster implements IPlayer {
+
+    private String name;
+    private ArrayList<ITreasure> treasureBag;
+    private int healthPoints;
+    private int progress;
+
+    public Warlock(String name, ArrayList treasureBag, int healthPoints, int progress, Spell spell) {
+        super(name, treasureBag, healthPoints, progress, spell);
+    }
 }
--- src/test/java/WarlockTest.java
+++ src/test/java/WarlockTest.java
@@ -1,2 +1,48 @@
+import Game.Abilities.Spell;
+import Game.Players.Warlock;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class WarlockTest {
+
+    Warlock warlock;
+    Spell thunderbolt;
+    Spell hailOfArrows;
+
+    @Before
+    public void before() {
+
+        thunderbolt = new Spell("Thunderbolt", 10);
+        hailOfArrows = new Spell("Hail of arrows", 20);
+        warlock = new Warlock("Saruman the White", null, 100, 0, thunderbolt);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Saruman the White", warlock.getName());
+    }
+
+    @Test
+    public void hasTreasure() {
+        assertEquals(0, warlock.getTreasure().size());
+    }
+
+    @Test
+    public void hasHealthPoints() {
+        assertEquals(100, warlock.getHealthPoints());
+    }
+
+    @Test
+    public void hasProgress() {
+        assertEquals(0, warlock.getProgress());
+    }
+
+    @Test
+    public void canSetSpell() {
+        warlock.setSpell(hailOfArrows);
+        assertEquals(hailOfArrows, warlock.getSpell());
+    }
+
 }
