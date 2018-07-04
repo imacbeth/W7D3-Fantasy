46210be4ac6e33c856ece0eefe93df03fab2df09
James Henderson <hendersonjamesalexander@gmail.com>
2018-07-04 13:28:16 +0100

    barbarian

--- src/main/java/Game/Players/Barbarian.java
+++ src/main/java/Game/Players/Barbarian.java
@@ -1,4 +1,22 @@
 package Game.Players;
 
-public class Barbarian {
-}
+import Game.Abilities.Weapon;
+import Game.Behaviours.IPlayer;
+import Game.Behaviours.ITreasure;
+
+import java.util.ArrayList;
+
+    public class Barbarian extends Fighter implements IPlayer {
+
+        private String name;
+        private ArrayList<ITreasure> treasureBag;
+        private  int healthPoints;
+        private int progress;
+
+        public Barbarian(String name, ArrayList treasureBag, int healthPoints, int progress, Weapon weapon){
+            super(name, treasureBag, healthPoints, progress, weapon);
+        }
+
+
+    }
+
--- src/test/java/BarbarianTest.java
+++ src/test/java/BarbarianTest.java
@@ -1,2 +1,48 @@
-public class BarbarianTest {
+import Game.Abilities.Weapon;
+import Game.Players.Barbarian;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+    public class BarbarianTest {
+
+        Barbarian barbarian;
+        Weapon axe;
+        Weapon sword;
+
+        @Before
+        public void before(){
+
+            axe = new Weapon("axe", 10);
+            sword = new Weapon("sword", 20);
+            barbarian = new Barbarian("Tony the Ruthless", null, 100, 0, axe);
+        }
+
+        @Test
+        public void hasName(){
+            assertEquals("Tony the Ruthless", barbarian.getName());
+        }
+
+        @Test
+        public void hasTreasure(){
+            assertEquals(0, barbarian.getTreasure().size());
+        }
+
+        @Test
+        public void hasHealthPoints(){
+            assertEquals(100, barbarian.getHealthPoints());
+        }
+
+        @Test
+        public void hasProgress(){
+            assertEquals(0, barbarian.getProgress());
+        }
+
+        @Test
+        public void canSetWeapon(){
+            barbarian.setWeapon(sword);
+            assertEquals(sword, barbarian.getWeapon());
+        }
 }
